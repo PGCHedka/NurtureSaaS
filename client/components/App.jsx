48ddd6122f14d0e53835842528d5e1e22982181a
Emily Chu <lin.emily.chu@gmail.com>
2023-01-06 15:45:31 -0600

    Teacher, Admin view

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Nav from '../components/Nav.jsx';
+import Teacher from '../containers/Teacher.jsx';
+import Admin from '../containers/Admin.jsx';
 
 const App = () => {
+  const userType = useSelector((state) => state.userInfo.type);
+  let currComponent;
+  userType === 'admin'
+    ? (currComponent = <Admin />)
+    : (currComponent = <Teacher />);
   return (
     <div id='main'>
       <Nav />
       <Routes>
         <Route path='/login' /> {/** login path */}
-        <Route path='/' /> {/** main path */}
+        <Route path='/' element={currComponent} /> {/** main path */}
         {/* <Route path='/profile' /> * profile path -- teacher only */}
       </Routes>
     </div>
