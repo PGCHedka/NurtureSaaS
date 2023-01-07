4212a67fd0d01e6c2ea02f81ce0412b76585048e
amlarouche <alarouche@gmail.com>
2023-01-06 20:03:27 -0800

    created base of teacherview component

--- client/components/Teacher.jsx
+++ client/components/Teacher.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Nav from '../components/Nav.jsx';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateFeed, addFriends } from '../rootReducer.js';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/main.scss';
