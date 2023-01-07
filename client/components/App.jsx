f4c458b8c42ab91c5477a3ad4d600d80ff669c95
Emily Chu <lin.emily.chu@gmail.com>
2023-01-06 18:29:00 -0600

    merge conflict fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Nav from '../components/Nav.jsx';
 import TeacherView from '../containers/TeacherView.jsx';
-import AdminView from '../containers/AdminView.jsx';
+import Admin from '../containers/AdminView.jsx';
 import Register from '../containers/Register.jsx';
 import LoginAdmin from '../containers/LoginAdmin.jsx';
 
@@ -11,23 +11,17 @@ const App = () => {
   const userType = useSelector((state) => state.userInfo.type);
   let currComponent;
   userType === 'admin'
-    ? (currComponent = <AdminView />)
+    ? (currComponent = <Admin />)
     : (currComponent = <TeacherView />);
   return (
     <div id='main'>
       <Nav />
       <Routes>
-<<<<<<< Updated upstream
         <Route path='/login' /> {/** login path */}
         <Route path='/dashboard' element={currComponent} /> {/** main path */}
         <Route path='/register' element={<Register />} />
         <Route path='/' element={<LoginAdmin />} /> {/** login path */}
         {/* <Route path='/profile' /> * profile path -- teacher only */}
-=======
-        <Route path='/dashboard' element={currComponent} /> {/** main path */}
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<LoginAdmin />} /> {/** login path */}
->>>>>>> Stashed changes
       </Routes>
     </div>
   );
