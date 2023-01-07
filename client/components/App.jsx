ef3b18e02e8d579e797856b807e3741b322086de
Emily Chu <lin.emily.chu@gmail.com>
2023-01-06 18:25:16 -0600

    admin styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -17,11 +17,17 @@ const App = () => {
     <div id='main'>
       <Nav />
       <Routes>
+<<<<<<< Updated upstream
         <Route path='/login' /> {/** login path */}
         <Route path='/dashboard' element={currComponent} /> {/** main path */}
         <Route path='/register' element={<Register />} />
         <Route path='/' element={<LoginAdmin />} /> {/** login path */}
         {/* <Route path='/profile' /> * profile path -- teacher only */}
+=======
+        <Route path='/dashboard' element={currComponent} /> {/** main path */}
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<LoginAdmin />} /> {/** login path */}
+>>>>>>> Stashed changes
       </Routes>
     </div>
   );
