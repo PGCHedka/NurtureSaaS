c03ed840239c8b41d896771e17117170f21ea7ac
L05Dhruv <dthota8@gmail.com>
2023-01-14 20:56:56 -0600

    server modification for deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const authRouter = require('./routes/authRouter');
 const adminRouter = require('./routes/adminRouter');
 const teacherRouter = require('./routes/teacherRouter');
